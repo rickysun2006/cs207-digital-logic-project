e6d7a62cf048e08c425550f7aad9d4b4102cedfa
DraTelligence <DraTelligence@outlook.com>
2025-12-09 20:01:29 +0800

    feat: 完善了随机数生成器，使其直接生成范围内的数

--- src/common/lfsr_core.sv
+++ src/common/lfsr_core.sv
@@ -17,19 +17,45 @@
 # v1.0  | 2025-12-09 | DraTelligence  |   Initial creation
 #
 #=============================================================================*/
+`include "../common/project_pkg.sv"
+import project_pkg::*;
 
-module lfsr_8bit (
-    input  wire       clk,
-    input  wire       rst_n,
-    input  wire       en,     // 使能信号
-    output reg  [7:0] dout    // 随机数输出
+module lfsr_core (
+    input wire clk,
+    input wire rst_n,
+    input wire en,
+
+    // --- 随机数范围 ---
+    input wire [7:0] cfg_min,
+    input wire [7:0] cfg_max,
+
+    // --- 输出 ---
+    output reg [7:0] rand_val  // 映射后的随机数
 );
+  reg [7:0] lfsr_raw;
 
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      dout <= 8'hFF;
+      lfsr_raw <= 8'hFF;
     end else if (en) begin
-      dout <= {dout[6:0], dout[7] ^ dout[5] ^ dout[4] ^ dout[3]};
+      lfsr_raw <= {lfsr_raw[6:0], lfsr_raw[7] ^ lfsr_raw[5] ^ lfsr_raw[4] ^ lfsr_raw[3]};
+    end
+  end
+
+  // rand_val = cfg_min + (lfsr_raw % (cfg_max - cfg_min + 1))
+
+  wire signed [8:0] range_len;
+  assign range_len = $signed(cfg_max) - $signed(cfg_min) + 9'sd1;
+
+  wire [7:0] offset;
+
+  assign offset = (range_len > 0) ? (lfsr_raw % unsigned'(range_len)) : 8'd0;
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      rand_val <= 8'd0;
+    end else if (en) begin
+      rand_val <= cfg_min + offset;
     end
   end
 
